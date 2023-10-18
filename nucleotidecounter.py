efcbebbf88281102ebe2609000fd46fb3f70e830
pfb21 <pfb21@info21.cshl.edu>
2023-10-18 11:28:13 -0400

    Python 3 problem set script to count the number of each nucleotide in a DNA sequence written in upper or lower case

---
+++ nucleotidecounter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+dna=input("DNA seq:")
+dna.upper()
+A_count=dna.count('A')
+T_count=dna.count('T')
+G_count=dna.count('G')
+C_count=dna.count('C')
+
+print('A:', A_count, '\nT:', T_count, '\nG:', G_count, '\nC:', C_count)
+
+
